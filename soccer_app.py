d742bd9da0f765fed059496719de6ca5f4a2c20d
chouchenglai <chouchenglai1981@gmail.com>
2026-05-11 06:04:38 +0800

    Update soccer_app.py

--- soccer_app.py
+++ soccer_app.py
@@ -756,4 +756,4 @@ with tab2:
                
 # --- 底部 ---
 st.divider()
-st.markdown("""<div style="color: #888; font-size: 0.9em; text-align: left; padding-bottom: 20px;">謹慎理財 信用至上<br>Copyright © 2026 周振來賽事球管理系統版權所有</div>""", unsafe_allow_html=True)
+st.markdown("""<div style="color: #888; font-size: 0.9em; text-align: left; padding-bottom: 20px;">謹慎理財 信用至上<br>Copyright © 2026 周振來賽事管理系統版權所有</div>""", unsafe_allow_html=True)
