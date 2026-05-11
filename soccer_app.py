465bb4bc75eb255ef16f0e425b134374301845e5
chouchenglai <chouchenglai1981@gmail.com>
2026-05-12 01:01:43 +0800

    Update soccer_app.py

--- soccer_app.py
+++ soccer_app.py
@@ -420,7 +420,7 @@ with tab2:
             admin_pwd = st.text_input("請輸入管理員密鑰", type="password", key="admin_auth_lock")
             
             # 驗證密碼
-            if admin_pwd == "Caiyun1031": 
+            if admin_pwd == "Caiyun1992": 
                 is_authenticated = True
                 st.success("🔓 驗證成功：管理操作功能已開啟。")
             elif admin_pwd != "":
