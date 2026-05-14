d2376a60cf05f03ade84ccada2c27cbd59ad07ec
chouchenglai <chouchenglai1981@gmail.com>
2026-05-15 06:17:11 +0800

    Add files via upload

--- pages/ccl-live.py
+++ pages/ccl-live.py
@@ -690,6 +690,7 @@ with tab2:
     💡 提示：未註冊帳號，登錄模擬數據，將不會保留，註冊帳號後，就能建立報表保存數據！
     </p>
     """, unsafe_allow_html=True)
+    st.write("")
     st.markdown("<h2 style='color:#1E90FF; font-weight:bold;'>📂 登錄會員管理中心</h2>", unsafe_allow_html=True)
     st.markdown("<hr style='border: 1px solid #1E90FF; margin-top: -10px;'>", unsafe_allow_html=True)     
   
