598f5ef23821f080731876ce2919ff44417198a3
chouchenglai <chouchenglai1981@gmail.com>
2026-05-16 03:41:24 +0800

    Add files via upload

--- pages/live.py
+++ pages/live.py
@@ -270,5 +270,9 @@ with tab_live:
             st.components.v1.iframe("https://live.titan007.com/indexall_big.aspx", height=800, scrolling=True)
                
 # --- 底部 ---
-st.divider()
-st.markdown("""<div style="color: #888; font-size: 0.9em; text-align: left; padding-bottom: 20px;">謹慎理財 信用至上<br>Copyright © 2026 CCL-Live 體育賽事管理系統版權所有</div>""", unCCL-Livesafe_allow_html=True)
+st.markdown("""
+<div style="color: #888; font-size: 0.9em; text-align: left; padding-bottom: 20px;">
+謹慎理財 信用至上<br>
+Copyright © 2026 CCL-Live 體育賽事管理系統版權所有
+</div>
+""", unsafe_allow_html=True)
