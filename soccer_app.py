3a7f54bad2d7886441c5a2160ccfdb666ef780ae
chouchenglai <chouchenglai1981@gmail.com>
2026-05-15 09:17:08 +0800

    Add files via upload

--- soccer_app.py
+++ soccer_app.py
@@ -159,10 +159,10 @@ src="data:image/jpg;base64,{logo_base64}">
 體育賽事模擬交易與分析平臺
 
 <div class="hero-desc">
-            <a href="/ccl-live#即時比分" style="color: white; text-decoration: none;">⚽ 即時比分</a> ｜ 
-            <a href="/ccl-live#歷史數據" style="color: white; text-decoration: none;">📊 歷史數據</a> ｜ 
-            <a href="/ccl-live#模擬倉管理" style="color: white; text-decoration: none;">📈 模擬倉管理</a> ｜ 
-            <a href="/ccl-live#交流討論" style="color: white; text-decoration: none;">💬 交流討論</a>
+            <a href="/ccl-live?target=live" style="color: white; text-decoration: none;">⚽ 即時比分</a> ｜ 
+            <a href="/ccl-live?target=hist" style="color: white; text-decoration: none;">📊 歷史數據</a> ｜ 
+            <a href="/ccl-live?target=sim" style="color: white; text-decoration: none;">📈 模擬倉管理</a> ｜ 
+            <a href="/ccl-live?target=chat" style="color: white; text-decoration: none;">💬 交流討論</a>
         </div>
     </div>
     """, unsafe_allow_html=True)
